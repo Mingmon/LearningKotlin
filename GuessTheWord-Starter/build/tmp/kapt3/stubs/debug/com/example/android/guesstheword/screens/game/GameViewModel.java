ea4d881c600ba8b9ebc8adf1c7e5951278ff240c
Mingmon <60010313@kmitl.ac.th>
2020-06-05 16:44:40 +0700

    05.2: LiveData and LiveData observers

--- GuessTheWord-Starter/build/tmp/kapt3/stubs/debug/com/example/android/guesstheword/screens/game/GameViewModel.java
+++ GuessTheWord-Starter/build/tmp/kapt3/stubs/debug/com/example/android/guesstheword/screens/game/GameViewModel.java
@@ -2,30 +2,26 @@ package com.example.android.guesstheword.screens.game;
 
 import java.lang.System;
 
-/**
- * ViewModel containing all the logic needed to run the game
- */
-@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000*\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\b\n\u0002\b\u0005\n\u0002\u0010\u000e\n\u0002\b\u0005\n\u0002\u0010!\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0005\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\b\u0010\u0011\u001a\u00020\u0012H\u0002J\b\u0010\u0013\u001a\u00020\u0012H\u0014J\u0006\u0010\u0014\u001a\u00020\u0012J\u0006\u0010\u0015\u001a\u00020\u0012J\b\u0010\u0016\u001a\u00020\u0012H\u0002R\u001a\u0010\u0003\u001a\u00020\u0004X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0005\u0010\u0006\"\u0004\b\u0007\u0010\bR\u001a\u0010\t\u001a\u00020\nX\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u000b\u0010\f\"\u0004\b\r\u0010\u000eR\u0014\u0010\u000f\u001a\b\u0012\u0004\u0012\u00020\n0\u0010X\u0082.\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0017"}, d2 = {"Lcom/example/android/guesstheword/screens/game/GameViewModel;", "Landroidx/lifecycle/ViewModel;", "()V", "score", "", "getScore", "()I", "setScore", "(I)V", "word", "", "getWord", "()Ljava/lang/String;", "setWord", "(Ljava/lang/String;)V", "wordList", "", "nextWord", "", "onCleared", "onCorrect", "onSkip", "resetList", "GuessTheWord-Starter_debug"})
+@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 1, d1 = {"\u00008\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0010\u000b\n\u0000\n\u0002\u0010\b\n\u0000\n\u0002\u0010\u000e\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0007\n\u0002\u0010!\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0007\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\b\u0010\u0014\u001a\u00020\u0015H\u0002J\b\u0010\u0016\u001a\u00020\u0015H\u0014J\u0006\u0010\u0017\u001a\u00020\u0015J\u0006\u0010\u0018\u001a\u00020\u0015J\u0006\u0010\u0019\u001a\u00020\u0015J\u0006\u0010\u001a\u001a\u00020\u0015J\b\u0010\u001b\u001a\u00020\u0015H\u0002R\u0014\u0010\u0003\u001a\b\u0012\u0004\u0012\u00020\u00050\u0004X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u0014\u0010\u0006\u001a\b\u0012\u0004\u0012\u00020\u00070\u0004X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u0014\u0010\b\u001a\b\u0012\u0004\u0012\u00020\t0\u0004X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u0017\u0010\n\u001a\b\u0012\u0004\u0012\u00020\u00050\u000b8F\u00a2\u0006\u0006\u001a\u0004\b\f\u0010\rR\u0017\u0010\u000e\u001a\b\u0012\u0004\u0012\u00020\u00070\u000b8F\u00a2\u0006\u0006\u001a\u0004\b\u000f\u0010\rR\u0017\u0010\u0010\u001a\b\u0012\u0004\u0012\u00020\t0\u000b8F\u00a2\u0006\u0006\u001a\u0004\b\u0011\u0010\rR\u0014\u0010\u0012\u001a\b\u0012\u0004\u0012\u00020\t0\u0013X\u0082.\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u001c"}, d2 = {"Lcom/example/android/guesstheword/screens/game/GameViewModel;", "Landroidx/lifecycle/ViewModel;", "()V", "_eventGameFinish", "Landroidx/lifecycle/MutableLiveData;", "", "_score", "", "_word", "", "eventGameFinish", "Landroidx/lifecycle/LiveData;", "getEventGameFinish", "()Landroidx/lifecycle/LiveData;", "score", "getScore", "word", "getWord", "wordList", "", "nextWord", "", "onCleared", "onCorrect", "onGameFinish", "onGameFinishComplete", "onSkip", "resetList", "GuessTheWord-Starter_debug"})
 public final class GameViewModel extends androidx.lifecycle.ViewModel {
-    @org.jetbrains.annotations.NotNull()
-    private java.lang.String word = "";
-    private int score = 0;
+    private final androidx.lifecycle.MutableLiveData<java.lang.String> _word = null;
+    private final androidx.lifecycle.MutableLiveData<java.lang.Integer> _score = null;
+    private final androidx.lifecycle.MutableLiveData<java.lang.Boolean> _eventGameFinish = null;
     private java.util.List<java.lang.String> wordList;
     
     @org.jetbrains.annotations.NotNull()
-    public final java.lang.String getWord() {
+    public final androidx.lifecycle.LiveData<java.lang.Integer> getScore() {
         return null;
     }
     
-    public final void setWord(@org.jetbrains.annotations.NotNull()
-    java.lang.String p0) {
+    @org.jetbrains.annotations.NotNull()
+    public final androidx.lifecycle.LiveData<java.lang.String> getWord() {
+        return null;
     }
     
-    public final int getScore() {
-        return 0;
-    }
-    
-    public final void setScore(int p0) {
+    @org.jetbrains.annotations.NotNull()
+    public final androidx.lifecycle.LiveData<java.lang.Boolean> getEventGameFinish() {
+        return null;
     }
     
     /**
@@ -51,9 +47,18 @@ public final class GameViewModel extends androidx.lifecycle.ViewModel {
     }
     
     /**
-     * Moves to the next word in the list.
+     * Moves to the next _word in the list.
      */
     private final void nextWord() {
+    }
+    
+    /**
+     * Method for the game completed event
+     */
+    public final void onGameFinish() {
+    }
+    
+    public final void onGameFinishComplete() {
     }
     
     public GameViewModel() {
